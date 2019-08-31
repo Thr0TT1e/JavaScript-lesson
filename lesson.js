846dbf195a9e541215901fc816b8f8e4d8f429a6
Thr0TT1e <wm@noble24.pro>
2019-08-31 19:28:20 +0300

    first commit

--- lesson.js
+++ lesson.js
@@ -1,4 +1,4 @@
-let money = prompt('Ваш бюджет на месяц?', ''),
+let money = +prompt('Ваш бюджет на месяц?', ''),
   time = prompt("Введите дату в формате YYYY-MM-DD", ''),
   appDate = {
     budget: money,
@@ -9,9 +9,9 @@ let money = prompt('Ваш бюджет на месяц?', ''),
     savings: false,
   },
   kredit1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-  answer1 = prompt('Во сколько обойдется?', ''),
+  answer1 = +prompt('Во сколько обойдется?', ''),
   kredit2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-  answer2 = prompt('Во сколько обойдется?', '');
+  answer2 = +prompt('Во сколько обойдется?', '');
 
 appDate.expenses.kredit1 = answer1;
 appDate.expenses.kredit2 = answer2;
